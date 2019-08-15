1afcfb26d7d0dc92e4ed652f98ad834b77a7f741
= <874857839@qq.com>
2019-08-15 11:48:04 +0800

    新建包

---
+++ src/main/java/com/kgc/chinaautorental/dao/T.java
@@ -0,0 +1,9 @@
+package com.kgc.chinaautorental.dao;
+
+/**
+ * @Author: LiKai
+ * @Description:
+ * @Date: Create in 11:46 2019/8/15
+ */
+public class T {
+}
---
+++ src/main/java/com/kgc/chinaautorental/service/T.java
@@ -0,0 +1,9 @@
+package com.kgc.chinaautorental.service;
+
+/**
+ * @Author: LiKai
+ * @Description:
+ * @Date: Create in 11:47 2019/8/15
+ */
+public class T {
+}
