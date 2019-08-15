2717c4f198f391aad463868456552aeb273ff675
= <874857839@qq.com>
2019-08-15 11:57:47 +0800

    新建ServiceImpl包

--- src/main/java/com/kgc/chinaautorental/dao/LoginMapper.java
+++ src/main/java/com/kgc/chinaautorental/dao/LoginMapper.java
@@ -1,9 +1,12 @@
 package com.kgc.chinaautorental.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 /**
  * @Author: LiKai
  * @Description:
  * @Date: Create in 11:50 2019/8/15
  */
-public class LoginMapper {
+public interface LoginMapper {
+    void login(@Param("phone") String phone, @Param("password") String )
 }
---
+++ src/main/java/com/kgc/chinaautorental/service/impl/T.java
@@ -0,0 +1,9 @@
+package com.kgc.chinaautorental.service.impl;
+
+/**
+ * @Author: LiKai
+ * @Description:
+ * @Date: Create in 11:57 2019/8/15
+ */
+public class T {
+}
